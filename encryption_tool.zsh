6edab90f455bec2623d13fb3273c26d9f2a2f5ef
Misak Tivriktsyan <tivriktsyanm@gmail.com>
2024-10-20 23:37:17 -0700

    Remove GCM and other incompatible enc algos for openssl for now. Add decryption functionality.

--- encryption_tool.zsh
+++ encryption_tool.zsh
@@ -17,22 +17,34 @@ main_menu() {
 
     case $choice in
         1) # Encrypt via symmetric encryption
-            echo -e "test 1 \n"
             echo "Enter full path of file or directory: "; read -r inputFile
             echo "Enter in the output file or directory name (for post encryption): "; read -r outputFile
             select_algorithm
 
+            # if [[ $algorithm == *"gcm"* || $algorithm == *"chacha20-poly1305"* ]]; then
+            #     key=$(openssl rand -hex 32)
+            #     iv=$(openssl rand -hex 12)
+
+            #     openssl enc -"$algorithm" -K "$key" -iv "$iv" -in "$inputFile" -out "$outputFile"
+
+            # else
             openssl enc -"$algorithm" -pbkdf2 -iter 1000000 -md sha256 -salt -in "$inputFile" -out "$outputFile"
+            # fi
 
             echo "File encrypted successfully using $algorithm!"
         ;;
 
         2) # Decrypt via symmetric encryption
-            echo -e "testing 2 \n"
+            echo "Enter full path of encrypted file or directory: "; read -r inputFile
+            echo "Enter in the output file or directory name (for post decryption): "; read -r outputFile
+            select_algorithm
+            openssl enc -"$algorithm" -d -pbkdf2 -iter 1000000 -md sha256 -salt -in "$inputFile" -out "$outputFile"
+
         ;;
 
         3) # Encrypt via public/private key
             echo -e "testing 3 \n"
+        ;;
 
         99) # Exit loop with exit code 0
             echo -e "Exiting! \n"
@@ -43,33 +55,36 @@ main_menu() {
 
 # Function for selecting algorithm
 select_algorithm(){
-    echo "Select encryption algorithm to use (showing only the top 10 recommended as of 2024): "
-    echo "1) AES-256-GCM"
-    echo "2) AES-128-GCM"
-    echo "3) ChaCha20-Poly1305"
-    echo "4) AES-256-CTR"
-    echo "5) AES-128-CTR"
-    echo "6) Camellia-256-CBC"
-    echo "7) AES-256-XTS"
-    echo "8) ARIA-256-GCM"
-    echo "9) AES-128-CBC"
-    echo "10) Camellia-128-GCM"
+    echo "Select encryption/decryption algorithm to use: "
+    echo "1) AES-128-CBC"
+    echo "2) AES-192-CBC"
+    echo "3) AES-256-CBC"
+    echo "4) AES-128-ECB"
+    echo "5) AES-192-ECB"
+    echo "6) AES-256-ECB"
+    echo "7) Camellia-256-CBC"
+    echo "8) AES-256-XTS*"
+    echo "9) ARIA-256-GCM*"
+    echo "10) AES-256-CTR"
+    echo "11) AES-128-CTR"
 
     read -r algorithmChoice
 
     case $algorithmChoice in
-        1) algorithm="aes-256-gcm" ;;
-        2) algorithm="aes-128-gcm" ;;
-        3) algorithm="chacha20-poly1305" ;;
-        4) algorithm="aes-256-ctr" ;;
-        5) algorithm="aes-128-ctr" ;;
-        6) algorithm="camellia-256-cbc" ;;
-        7) algorithm="aes-256-xts" ;;
-        8) algorithm="aria-256-gcm" ;;
-        9) algorithm="aes-128-cbc" ;;
-        10) algorithm="camellia-128-gcm" ;;
+        1) algorithm="aes-128-cbc" ;;
+        2) algorithm="aes-192-cbc" ;;
+        3) algorithm="aes-256-cbc" ;;
+        4) algorithm="aes-128-ecb" ;;
+        5) algorithm="aes-192-ecb" ;;
+        5) algorithm="aes-256-ecb" ;;
+        7) algorithm="camellia-256-cbc" ;;
+        8) algorithm="aes-256-xts" ;;
+        9) algorithm="aria-256-gcm" ;;
+        10) algorithm="aes-256-ctr" ;;
+        11) algorithm="aes-128-ctr" ;;
         *) echo "Invalid selection!"; exit 1 ;;
     esac
+    echo ""
     # echo "$algorithm"
 }
 
