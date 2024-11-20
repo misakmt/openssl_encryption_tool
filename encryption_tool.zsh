0895b048f7e50a99c442847efb7022da0a8925f0
Misak Tivriktsyan <tivriktsyanm@gmail.com>
2024-11-20 00:08:22 -0800

    Adding rest of functionality -- assymetric enc and cert check

--- encryption_tool.zsh
+++ encryption_tool.zsh
@@ -2,13 +2,14 @@
 
 # Display main menu
 main_menu() {
+    echo "" 
     echo "Please make a selection to get started: "
     echo "1) Encrypt files via symmetric encryption"
-    echo "2) Decrypt files via symmetric encryption"
-    echo "3) Encrypt files via public/private key"
-    echo "4) Decrypt files via public/private key"
-    echo "5) Check certificate"
-    echo "6) Generate hash"
+    echo "2) Decrypt files via symmetric decryption"
+    echo "3) Encrypt files via asymmetric encryption"
+    echo "4) Decrypt files via asymmetric decryption"
+    echo "5) Generate Hash"
+    echo "6) Check Certificate"
     echo "7) GPG menu"
     echo -e "99 Quit application \n"
 
@@ -40,10 +41,53 @@ main_menu() {
             select_algorithm
             openssl enc -"$algorithm" -d -pbkdf2 -iter 1000000 -md sha256 -salt -in "$inputFile" -out "$outputFile"
 
+            echo "File successfully decrypted"; 
+            ls -l $outputFile
+
         ;;
 
         3) # Encrypt via public/private key
-            echo -e "testing 3 \n"
+            echo "Enter in full path of recipient's public key: "; read -r publicKey
+            echo "Enter in full path of file to encrypt: "; read -r inputFile
+            echo "What do you want the name of the output file to be? Please use full path again: "; read -r outputFile
+            openssl pkeyutl -encrypt -inkey "$publicKey" -pubin -in "$inputFile" -out "$outputFile"
+
+            echo "File successfully encrypted"; 
+            ls -l $outputFile
+
+        ;;
+
+        4) # Decrypt via public/private key
+            echo "Enter in full path of your private key: "; read -r privateKey
+            echo "Enter in full path of file to decrypt: "; read -r inputFile
+            echo "What do you want the name of the output file to be? Please use full path again: "; read -r outputFile
+            openssl pkeyutl -decrypt -inkey "$privateKey" -in "$inputFile" -out "$outputFile"
+
+            echo "File successfully decrypted"; 
+            ls -l $outputFile
+
+        ;;
+
+        5) # Generate and check hashes
+            echo "Enter full path of file or directory: "; read -r inputFile
+            select_hash
+            openssl dgst -"$hashAlgorithm" "$inputFile"
+        ;;
+
+        6) # Check the certificate provided
+            echo "Enter full path of certificate: "; read -r inputFile
+            echo "Are you checking an rsa key or x509 certificate? Type in 'rsa' or 'x509': "; read -r certType
+            echo
+
+            if [[ $certType == "x509" ]]; then
+                openssl x509 -in "$inputFile" -text -noout
+
+            elif [[ $certType == "rsa" ]]; then
+                openssl rsa -in "$inputFile" -text -noout
+            
+            else
+                echo "Did not receive rsa or x509 choice! Please try again."
+            fi
         ;;
 
         99) # Exit loop with exit code 0
@@ -82,10 +126,57 @@ select_algorithm(){
         9) algorithm="aria-256-gcm" ;;
         10) algorithm="aes-256-ctr" ;;
         11) algorithm="aes-128-ctr" ;;
-        *) echo "Invalid selection!"; exit 1 ;;
+        *) echo "Invalid selection! Please try again. "; select_algorithm ;;
     esac
-    echo "" #spacing for readability
+    echo " " #spacing for readability
     # echo "$algorithm"
+}
+
+select_hash(){
+    echo "Select hash function to use: "
+    echo "1) blake2b512"
+    echo "2) blake2s256"
+    echo "3) md5"
+    echo "4) rmd160"
+    echo "5) sha1"
+    echo "6) sha224"
+    echo "7) sha256"
+    echo "8) sha3-224"
+    echo "9) sha3-256"
+    echo "10) sha3-384"
+    echo "11) sha3-512"
+    echo "12) sha384"
+    echo "13) sha512"
+    echo "14) sha512-224"
+    echo "15) sha512-256"
+    echo "16) shake128"
+    echo "17) shake256"
+    echo "18) sm3"
+
+    read -r hashAlgorithmChoice
+
+    case $hashAlgorithmChoice in
+        1) hashAlgorithm="blake2b512" ;;
+        2) hashAlgorithm="blake2s256" ;;
+        3) hashAlgorithm="md5" ;;
+        4) hashAlgorithm="rmd160" ;;
+        5) hashAlgorithm="sha1" ;;
+        5) hashAlgorithm="sha224" ;;
+        7) hashAlgorithm="sha256" ;;
+        8) hashAlgorithm="sha3-224" ;;
+        9) hashAlgorithm="sha3-256" ;;
+        10) hashAlgorithm="sha3-384" ;;
+        11) hashAlgorithm="sha3-512" ;;
+        12) hashAlgorithm="sha384" ;;
+        13) hashAlgorithm="sha512" ;;
+        14) hashAlgorithm="sha512-224" ;;
+        15) hashAlgorithm="sha512-256" ;;
+        16) hashAlgorithm="shake128" ;;
+        17) hashAlgorithm="shake256" ;;
+        18) hashAlgorithm="sm3" ;;
+        *) echo "Invalid selection! Please try again. "; select_hash ;;
+    esac
+    echo " " #spacing for readability
 }
 
 
